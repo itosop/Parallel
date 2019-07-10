5b0e0c2619cb95ed6ee53ed4945305634ec0f9c8
itosop <souheiheisei39@gmail.com>
2019-07-10 14:53:07 +0900

    change function of 'file_input', and fix bug a little

--- dp.c
+++ dp.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define Content_N 10000
-#define Max_V 100
-#define BUF_SIZE 32
+#define Max_V 200
+#define BUF_SIZE 16
 
 /*-------------- Function ------------------------*/
 int file_input(char [], int [][2]);
@@ -14,14 +14,15 @@ int main(int argc, char *argv[]){
   int n, v;
   int i, j; //ループ変数
   int cost[Content_N][2];
-  int ans[Content_N][Max_V];
+  int ans[Content_N+1][Max_V+1];
   
   n = file_input(argv[1], cost);
   if(n == -1){
     return -1;
   }
-  printf("Please input volume : "); scanf("%d", &v);
 
+  printf("Please input volume : "); scanf("%d", &v);
+  
   for(j = 0; j <= v; j++){
     ans[n][j] = 0;
   }
@@ -35,48 +36,27 @@ int main(int argc, char *argv[]){
       }
     }
   }
-
-  printf("%d\n", ans[0][v-1]);
-
+  
+  printf("%d\n", ans[0][v]);
+ 
   return 0;
 }
 /*-------------------------------------------------*/
 
 int file_input(char filename[], int cost[][2]){
   FILE *fp;
-  char buf[Content_N][BUF_SIZE], buf2[BUF_SIZE];
-  int line = 0, i, j, k, l, m;
+  int line = 0, ret;
   
   fp = fopen(filename, "r");
   if(fp == NULL){
     printf("%s is empty.\n", filename);
     return -1;
   }
-  
-  while(fgets(buf[line], BUF_SIZE, fp) != NULL){
+
+  while((ret=fscanf(fp, "%d,%d", &cost[line][0], &cost[line][1])) != EOF){   
     line++;
   }
-  for(i = 0; i < line; i++){
-    j = 0;
-    k = 0;
-    l = 0;
-    while(buf[i][j] != '\0'){
-      if(buf[i][j] == ',' || buf[i][j] == '\n'){
-	cost[i][k] = atoi(buf2);
-	k++;
-	j++;
-	l = 0;
-	for(m = 0; m < BUF_SIZE; m++){
-	  buf2[m] = '\0';
-	}
-      }
-      else{
-	buf2[l] = buf[i][j];
-	j++;
-	l++;
-      }
-    }
-  }
+  fclose(fp);
  
   return line;
 }
