24b80b15d0a132b243d7e064f5eb5476aa687e48
itosop <souheiheisei39@gmail.com>
2019-07-10 19:52:11 +0900

    Add DP program for C using OpenMP

--- dp_omp_para.c
+++ dp_omp_para.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h> 
 #define Content_N 10000
 #define Max_V 10000
 #define BUF_SIZE 16
@@ -17,7 +18,7 @@ int max(int, int);
 int main(int argc, char *argv[]){
   int n, v;
   int i, j; //ループ変数
-  clock_t start, end;
+  struct timespec startTime, endTime;
   
   n = file_input(argv[1], cost);
   if(n == -1){
@@ -26,13 +27,15 @@ int main(int argc, char *argv[]){
 
   printf("Please input volume : "); scanf("%d", &v);
 
-  start = clock();
+  /* 時間計測 開始 */
+  clock_gettime(CLOCK_REALTIME, &startTime);
   
   for(j = 0; j <= v; j++){
     ans[n][j] = 0;
   }
  
   for(i = n-1; i >= 0; i--){
+    #pragma omp parallel for private(j)
     for(j = 0; j <= v; j++){
       if(j < cost[i][0]){
 	ans[i][j] = ans[i+1][j];
@@ -43,10 +46,21 @@ int main(int argc, char *argv[]){
     }
   }
 
-  end = clock();
+  /* 時間計測 終了 */
+  clock_gettime(CLOCK_REALTIME, &endTime);
   
-  printf("%d\n", ans[0][v]);
-  printf("%f (sec)\n", (double)(end-start)/CLOCKS_PER_SEC);
+  printf("Ans = %d\n", ans[0][v]);
+  
+  printf("elapsed time = ");
+  if (endTime.tv_nsec < startTime.tv_nsec) {
+    printf("%5ld.%09ld", endTime.tv_sec - startTime.tv_sec - 1,
+	   endTime.tv_nsec + (long int)1.0e+9 - startTime.tv_nsec);
+  }
+  else {
+    printf("%5ld.%09ld", endTime.tv_sec - startTime.tv_sec,
+	   endTime.tv_nsec - startTime.tv_nsec);
+  }
+  printf("(sec)\n");
   
   return 0;
 }
